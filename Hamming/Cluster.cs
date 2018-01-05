fe8fddeb718ec233bb462e613026da483f7d0998
franckentien <franckentien@hotmail.fr>
2018-01-05 23:25:51 +0100

    Make structure of cluster for 2 values

--- Hamming/Cluster.cs
+++ Hamming/Cluster.cs
@@ -2,6 +2,42 @@
 {
     public class Cluster
     {
-        
+
+
+        public static int[] ExtractLine(int[,] hammingTab)
+        {
+            int[] rst = null;
+            if (hammingTab.GetLength(0) <= 2)
+            {
+                rst = new[] { 0, 1 };
+            }
+            else
+            {
+                rst = GetHigherHamming(hammingTab)  ;
+            }
+
+            return rst;
+        }
+
+        public static int[] GetHigherHamming(int[,] hammingTab)
+        {
+            int[] rst = {0, 1};
+
+            return rst;
+        }
+
+        public static int [,] BuildCluster (int[,] hammingTab)
+        {
+            int[,] cluster = null;
+            if (hammingTab.GetLength(0) == 2)
+            {
+                cluster = new int[,] { { hammingTab[0, 0] }, { hammingTab[1, 0] } };
+
+            }
+
+            return cluster;
+        }
+
+
     }
 }
--- HammingTest/ClusterTest.cs
+++ HammingTest/ClusterTest.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using  Hamming;
 
 namespace HammingTest
 {
     [TestClass]
     public class ClusterTest
     {
+
         [TestMethod]
-        public void TestConstructorCopyMatrice()
+        public void TestExtractLine2LineHamming0()
         {
-            Assert.Fail();
+            int[,] hammingTab = { { 0, 0 }, { 0, 0 } };
+
+            int[] line = Cluster.ExtractLine(hammingTab);
+
+            int[] result = { 0, 1 };
+            Assert.AreEqual(string.Join(",", line), string.Join(",", result));
         }
+
+        [TestMethod]
+        public void TestExtractLine3LineHamming0()
+        {
+            int[,] hammingTab = { { 0, 0,0 }, { 0, 0, 0 }, { 0, 0, 0} };
+
+            int[] line = Cluster.ExtractLine(hammingTab);
+
+            int[] result = { 0, 1 };
+            Assert.AreEqual(string.Join(",", line), string.Join(",", result));
+        }
+
+
+
+
+        [TestMethod]
+        public void TestBuild2ClusterWith2ValueHamming0()
+        {
+            int [,] hammingTab = {{1,0},{2,0}};
+
+            int[,] cluster = Cluster.BuildCluster(hammingTab);
+
+            int[,] result = {{1}, {2}};
+            Assert.AreEqual(Program.Print2DArray(cluster),Program.Print2DArray(result));
+        }
+
+
     }
 }
